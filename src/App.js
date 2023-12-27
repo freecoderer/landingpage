bd1fa1706c08de4cd8a8300d0ca89676c44769be
freecoderer <donggood7@naver.com>
2023-12-27 16:49:30 +0900

    feat: make a first component and bottoms

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className={"first"}>
+              <div className={"firsthook"}>
+                  <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="138" height="81" viewBox="0 0 138 81" fill="none">
+                      <path
+                          d="M128.143 0.366608H98.5714L78.8571 32.2688V80.1222H138V32.2688H108.429L128.143 0.366608ZM49.2857 0.366608H19.7143L0 32.2688V80.1222H59.1429V32.2688H29.5714L49.2857 0.366608Z"
+                          fill="#2B2B2B"/>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="138" height="80" viewBox="0 0 138 80" fill="none">
+                      <path
+                          d="M9.85714 79.7556L39.4286 79.7556L59.1429 47.8534L59.1429 1.01251e-05L4.2759e-06 5.91727e-06L-8.64562e-07 47.8534L29.5714 47.8534L9.85714 79.7556ZM88.7143 79.7556L118.286 79.7556L138 47.8534L138 1.57356e-05L78.8571 1.15278e-05L78.8571 47.8534L108.429 47.8534L88.7143 79.7556Z"
+                          fill="#2B2B2B"/>
+                  </svg>
+                  </div>
+                  <div className={"text1"}>실례지만 어떤 음악 좋아하세요?</div>
+              </div>
+              <div className={"scrollin"}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="98" height="30" viewBox="0 0 98 30" fill="none">
+                      <path d="M3 3L48.5756 26.6149" stroke="#505050" stroke-width="5" stroke-linecap="round"/>
+                      <path d="M94.5756 3L49 26.6149" stroke="#505050" stroke-width="5" stroke-linecap="round"/>
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="98" height="30" viewBox="0 0 98 30" fill="none">
+                      <path d="M3 3L48.5756 26.6149" stroke="#C2C2C2" stroke-width="5" stroke-linecap="round"/>
+                      <path d="M94.5756 3L49 26.6149" stroke="#C2C2C2" stroke-width="5" stroke-linecap="round"/>
+                  </svg>
+              </div>
+          </div>
+          <div className={"second"}>
+
+
+          </div>
+
+      </>
   );
 }
 
