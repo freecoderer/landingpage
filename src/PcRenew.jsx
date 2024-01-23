c609b40bf2c278d2b62ce58067cc6369d32b5870
freecoderer <donggood7@naver.com>
2024-01-23 11:55:59 +0900

    feat : completely modified jsx design to figma

--- src/PcRenew.jsx
+++ src/PcRenew.jsx
@@ -3,6 +3,7 @@ import emailjs from '@emailjs/browser';
 import logo from './logo.svg';
 import './PcRenew.css';
 import hookimg from './Group 33.svg';
+import highlight from './highlight.png';
 function Renew() {
     const [language, setLanguage] = useState('ko'); // default language is English
     const toggleLanguage = () => {
@@ -27,15 +28,32 @@ function Renew() {
         <div  className={"backgrounds"}>
             <div className={"navnav"}>
                 <img src={logo} alt="Logo" className={"logo"}/>
-                <button className={"btn"} onClick={toggleLanguage}>
-                    {language === 'en' ? 'Change language to Korean' : 'Change language to English'}
+                <button  onClick={toggleLanguage} className="w-[6rem] h-[2.7rem] ml-1 bg-neutral-700 rounded-[0.5rem] flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none"
+                         className={"ml-1"}>
+                        <g clip-path="url(#clip0_1_10)">
+                            <path
+                                d="M1.91667 11.5C1.91667 16.7929 6.20713 21.0833 11.5 21.0833C16.7929 21.0833 21.0833 16.7929 21.0833 11.5C21.0833 6.20712 16.7929 1.91666 11.5 1.91666C6.20713 1.91666 1.91667 6.20712 1.91667 11.5Z"
+                                stroke="white" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path
+                                d="M12.4583 1.96457C12.4583 1.96457 15.3333 5.74999 15.3333 11.5C15.3333 17.25 12.4583 21.0354 12.4583 21.0354M10.5417 21.0354C10.5417 21.0354 7.66667 17.25 7.66667 11.5C7.66667 5.74999 10.5417 1.96457 10.5417 1.96457M2.52042 14.8542H20.4796M2.52042 8.14582H20.4796"
+                                stroke="white" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_1_10">
+                                <rect width="23" height="23" fill="white"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                    <div className={"w-[0.7rem]"}></div>
+                    <div><p className={"text-amber-50"}>{language === 'en' ? '한국어' : 'English'}</p></div>
                 </button>
             </div>
             <div className={"firsthook"}>
                 <div className={"firsthook1"}>
-                <p className={"texto"}>{language === 'en' ? 'The place where I can find my music taste, mylist' : '나만의 음악 취향이'}</p>
-                <p className={"texto"}>{language === 'en' ? '' : '완성되는 공간, 마이리스트'}</p>
-                <p className={"texto2"}>Create and Share Your Playlist!</p>
+                    <p className={"texto"}>{language === 'en' ? 'The place where I can find my music taste mylist' : '나만의 음악 취향이'}</p>
+                    <p className={"texto"}>{language === 'en' ? '' : '완성되는 공간, 마이리스트'}</p>
+                    <p className={"texto2"}>Create and Share Your Playlist!</p>
                 </div>
                 <div className={"hookimg"}>
                     <img src={hookimg} alt="Logo" className={"hookimg"}/>
@@ -45,7 +63,9 @@ function Renew() {
             <div className={"secondhook"}>
                 <div className={"subsecond"}>
                     <p className={"texto21"}>{language === 'en' ? 'If you share each others music taste' : '서로의 음악 취향이 궁금할 때'}</p>
-                    <p className={"texto22"}>{language === 'en' ? 'You can easily share them with just a link in your profile' : '프로필 링크 하나로 간편하게'}</p>
+                    <div className={"subsubsecond"}>
+                        <p className={"texto22"}>{language === 'en' ? 'You can easily share them with just a link in your PROFILE' : '프로필 링크 하나로 간편하게'}</p>
+                    </div>
                 </div>
             </div>
 
@@ -85,8 +105,10 @@ function Renew() {
                 <div className={"fourthsubtit"}>
                     <p className={"fourthname"}>{language === 'en' ? 'Please Join on Waitlist' : '대기 명단 등록'}</p>
                 </div>
+                <br></br>
                 <div className={"fourthmaintit"}>
-                    <p className={"fourthdesc"}>{language === 'en' ? 'If you want to use our product' : '가장 먼저 마이리스트를'}</p>
+                    <img src={highlight} alt="highlight" className={"highlight"}/>
+                    <p className={"fourthdesc"}>{language === 'en' ? 'If you want to join in mylist' : '가장 먼저 마이리스트를'}</p>
                     <p className={"fourthdesc"}>{language === 'en' ? 'First on the earth' : '사용하고 싶다면?'}</p>
                 </div>
                 <div>
@@ -99,6 +121,9 @@ function Renew() {
                                 className="email-input-button">{language === 'en' ? 'Send' : '보내기'}</button></div>
                     </form>
                 </div>
+                <div className={"barbar"}></div>
+                <div className={"firstinfo"}><p className={"infoo"}>문의 : mylist.company@gmail.com</p></div>
+                <div className={"secondinfo"}><p className={"infoo"}>MyList Copyright ⓒ TEAM CRUSH. All Rights Reserved</p> </div>
             </div>
         </div>
         </div>
